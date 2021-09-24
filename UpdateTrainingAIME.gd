55294892d981e053e4991d018e50e2f44f1e0113
bkf2020 <74793713+bkf2020@users.noreply.github.com>
2021-09-23 21:51:57 -0700

    Connect timer to main script instead of a seperate script - The timer used to be connected to TrainingTimer.gd - Now it is connected to UpdateTrainingAIME.gd and UpdateTrainingAMC.gd - This is so the TimeLeft label is on the panel

--- UpdateTrainingAIME.gd
+++ UpdateTrainingAIME.gd
@@ -8,6 +8,28 @@ func _ready():
 	get_node("Problem4Description").text = PlayerVariables.problem_names[3]
 	get_node("Problem5Description").text = PlayerVariables.problem_names[4]
 
+func _physics_process(delta):
+	var tot_seconds = int(floor(get_node("Timer").time_left))
+	var seconds = tot_seconds % 60
+	tot_seconds = tot_seconds / 60 # get number of minutes
+	var minutes = tot_seconds % 60
+	tot_seconds = tot_seconds / 60 # get number of hours
+	var hours = tot_seconds
+	
+	var seconds_str = str(seconds)
+	var minutes_str = str(minutes)
+	var hours_str = str(hours)
+	
+	if(seconds < 10):
+		seconds_str = "0" + seconds_str
+	if(minutes < 10):
+		minutes_str = "0" + minutes_str
+	if(hours < 10):
+		hours_str = "0" + hours_str
+	
+	var display = "Time left: " + hours_str + ":" + minutes_str + ":" + seconds_str
+	get_node("TimeLeft").text = display
+
 func _on_ProblemLink_pressed(index):
 	OS.set_clipboard(PlayerVariables.problem_links[index])
 
